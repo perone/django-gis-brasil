e0cca03816be077ecc12e750e8645d965d686346
Christian S. Perone <christian.perone@gmail.com>
2013-06-16 18:34:24 -0300

    Incrementando versão após lançamento da v0.1

--- gisbrasil/__init__.py
+++ gisbrasil/__init__.py
@@ -1,6 +1,6 @@
 # happy fish-coding: utf-8
 
-__version__ = "0.1"
+__version__ = "0.2"
 __author__ = "CodeFish Team (Christian S. Perone, Leandro Nunes, Gabriel Wainer)"
 
 import loader
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-gis-brasil',
-    version='0.1',
+    version='0.2',
     packages=['gisbrasil'],
     include_package_data=True,
     license='BSD License',
