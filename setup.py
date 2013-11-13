adc1d2cb7c63895fb2066957f102b45f871c8d40
Christian S. Perone <christian.perone@eldorado.org.br>
2013-11-12 23:32:00 -0100

    Adicionando dataset do opendatapoa de acidentes de transito de Porto Alegre/RS.

--- setup.py
+++ setup.py
@@ -2,9 +2,11 @@ import os
 from setuptools import setup
 
 README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
-
-# allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+install_requirements = [
+        'progressbar2>=2.6.0',
+]
 
 setup(
     name='django-gis-brasil',
@@ -15,6 +17,7 @@ setup(
     zip_safe=False,
     description='Django GIS Brasil is a Django app (GeoDjango) with Brazilian GIS information.',
     long_description=README,
+    install_requires=install_requirements,
     url='https://github.com/perone/django-gis-brasil',
     author='CodeFi.sh Team (Christian S. Perone, Leandro Nunes, Gabriel Wainer)',
     author_email='christian.perone@gmail.com',
